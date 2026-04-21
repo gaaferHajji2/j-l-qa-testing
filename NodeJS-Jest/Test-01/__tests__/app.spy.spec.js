8abddbf113d487ec78f4d45f681e9405b0214662
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-21 13:39:09 +0300

    Refactor

--- NodeJS-Jest/Test-01/__tests__/app.spy.spec.js
+++ NodeJS-Jest/Test-01/__tests__/app.spy.spec.js
@@ -1,9 +1,10 @@
 const crypto = require('node:crypto')
 const { getData } = require('../app')
 
-jest.spyOn(crypto, 'randomBytes').mockImplementationOnce(()=>Promise.resolve('JLoka'))
 
 test('testing spyOn', async () => {
+    jest.spyOn(crypto, 'randomBytes').mockImplementationOnce(()=>Promise.resolve('JLoka'))
+
     const t1 = await getData()
     expect(t1).toEqual('JLoka')
 })

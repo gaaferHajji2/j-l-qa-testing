dcc1b2711b033b1a3088ad567730cbf4151cff36
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-21 13:48:19 +0300

    Test it with describe

--- NodeJS-Jest/Test-01/__tests__/group.spec.js
+++ NodeJS-Jest/Test-01/__tests__/group.spec.js
@@ -4,13 +4,13 @@ const { getData } = require('../app')
 jest.mock('node:crypto')
 
 describe('test mocking', () => {
-    test('Mocking function', async () => {
+    it('Mocking function', async () => {
         crypto.randomBytes.mockResolvedValueOnce('bytes')
         const res = await getData()
         expect(res).toEqual('bytes')
     })
 
-    test('Mocking function', async () => {
+    it('Mocking function', async () => {
         crypto.randomBytes.mockImplementationOnce(() => Promise.resolve('bytes'))
         const res = await getData()
         expect(res).toEqual('bytes')
@@ -18,7 +18,7 @@ describe('test mocking', () => {
 })
 
 describe('test spyOn', () => {
-    test('testing spyOn', async () => {
+    it('testing spyOn', async () => {
         jest.spyOn(crypto, 'randomBytes').mockImplementationOnce(()=>Promise.resolve('JLoka'))
     
         const res = await getData()

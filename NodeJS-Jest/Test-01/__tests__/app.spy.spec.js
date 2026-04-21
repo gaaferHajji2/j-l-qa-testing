3e18ebbf0b425b65ff6025892e0babfdeb6a2e50
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-21 13:40:59 +0300

    Change variable name

--- NodeJS-Jest/Test-01/__tests__/app.spy.spec.js
+++ NodeJS-Jest/Test-01/__tests__/app.spy.spec.js
@@ -1,10 +1,9 @@
 const crypto = require('node:crypto')
 const { getData } = require('../app')
 
-
 test('testing spyOn', async () => {
     jest.spyOn(crypto, 'randomBytes').mockImplementationOnce(()=>Promise.resolve('JLoka'))
 
-    const t1 = await getData()
-    expect(t1).toEqual('JLoka')
+    const res = await getData()
+    expect(res).toEqual('JLoka')
 })

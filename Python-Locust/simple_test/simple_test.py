018a6d47928ac3a96913e6e918f89db7581a31e6
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-22 16:36:07 +0300

    Adding simple test with weight and multiple task

--- Python-Locust/simple_test/simple_test.py
+++ Python-Locust/simple_test/simple_test.py
@@ -4,6 +4,7 @@ class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get('/hello')
+
     @task
     def hello_world_02(self):
         self.client.get('/world')

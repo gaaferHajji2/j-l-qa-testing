797564055ce4c3e7b6195b0bfd91b2df01d49a8f
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-22 16:55:30 +0300

    Adding comment

--- Python-Locust/simple_test/quick_test.py
+++ Python-Locust/simple_test/quick_test.py
@@ -3,12 +3,13 @@ from locust import HttpUser, task, between
 
 class QuickStartUser(HttpUser):
     wait_time = between(1, 10)
-    
+
     @task
     def hello_world(self):
         self.client.get("/hello")
         self.client.get("/world")
 
+    # maximize the chance to choose this task by 3-times
     @task(3)
     def view_items(self):
         for item_id in range(10):

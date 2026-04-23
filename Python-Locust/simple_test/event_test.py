9dbf4ea790cf635d2a9c99fe25aebbeaab6e49b7
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-23 16:52:55 +0300

    Writing simple event test example

---
+++ Python-Locust/simple_test/event_test.py
@@ -0,0 +1,18 @@
+from locust import events, task, HttpUser
+
+@events.test_start.add_listener
+def on_start_test(environment, **kwargs):
+    print("The test has begin")
+
+@events.test_stop.add_listener
+def on_stop_test(environment, **kwargs):
+    print("The test has been finished")
+
+class SimpleEvent(HttpUser):
+    @task(3)
+    def task_01(self):
+        self.client.get('/')
+
+    @task
+    def task_02(self):
+        self.client.post('/')

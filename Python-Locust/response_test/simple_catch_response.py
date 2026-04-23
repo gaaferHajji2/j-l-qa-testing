203fe743ecc0278e299246db0bd077e4ed106b8a
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-23 17:18:11 +0300

    Adding simple catch response with testing logic

---
+++ Python-Locust/response_test/simple_catch_response.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task, between
+
+class HomePage(HttpUser):
+    @task
+    def hello_01(self):
+        with self.client.get('/', catch_response=True) as response:
+            if response.elapsed.total_seconds()> 3:
+                response.failure("Too Long Time: Task-01")
+    @task
+    def hello_02(self):
+        with self.client.get('/about', catch_response=True) as response:
+            if response.elapsed.total_seconds() > 3:
+                response.failure("Too Long Time: Task:-02")
+            elif response.status_code == 404:
+                response.success()

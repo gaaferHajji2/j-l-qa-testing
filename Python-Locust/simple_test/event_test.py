1391d3d5178c29767f78c4d6b3eb138e4b6ddc5b
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-23 16:58:41 +0300

    Adding init event

--- Python-Locust/simple_test/event_test.py
+++ Python-Locust/simple_test/event_test.py
@@ -1,4 +1,5 @@
 from locust import events, task, HttpUser
+from locust.runners import MasterRunner
 
 @events.test_start.add_listener
 def on_start_test(environment, **kwargs):
@@ -7,6 +8,14 @@ def on_start_test(environment, **kwargs):
 @events.test_stop.add_listener
 def on_stop_test(environment, **kwargs):
     print("The test has been finished")
+
+@events.init.add_listener
+def on_locust_init(environment, **kwargs):
+    if isinstance(environment.runner, MasterRunner):
+        print("I'm on master node")
+    else:
+        print("I'm on a worker or standalone node")
+
 class SimpleEvent(HttpUser):
     @task(3)
     def task_01(self):

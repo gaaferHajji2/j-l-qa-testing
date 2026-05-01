f205ab037007e263b158e5975389c8fd30b50c6a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-01 23:24:43 +0300

    Create first spider

---
+++ Scrapy-Project/bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        pass

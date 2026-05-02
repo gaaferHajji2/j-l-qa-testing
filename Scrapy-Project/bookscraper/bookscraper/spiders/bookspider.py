9106daa90fa7640ccea1567725a3a34b428fd562
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 21:49:07 +0300

    Adding simple scrape test

--- Scrapy-Project/bookscraper/bookscraper/spiders/bookspider.py
+++ Scrapy-Project/bookscraper/bookscraper/spiders/bookspider.py
@@ -6,4 +6,10 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+        for book in books:
+            yield{
+                'name': book.css('h3 a::text').get(),
+                'price': book.css('.product_price .price_color::text').get(),
+                'url': books.css('h3 a').attrib['href']
+            }

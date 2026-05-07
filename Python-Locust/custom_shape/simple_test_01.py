1c08be3f9b7392aaf7bfe4b3e8fa93232c71ea51
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-07 17:57:56 +0300

    Adding custom shape with custom user classes

--- Python-Locust/custom_shape/simple_test_01.py
+++ Python-Locust/custom_shape/simple_test_01.py
@@ -4,3 +4,13 @@ class MySimpleTest(HttpUser):
     @task
     def simple_task_01(self):
         self.client.get('/')
+    
+class UserA(HttpUser):
+    @task
+    def simple_task_01(self):
+        self.client.get('/')
+
+class UserB(HttpUser):
+    @task
+    def simple_task_01(self):
+        self.client.get('/')
---
+++ Python-Locust/custom_shape/stages_shape_with_custom_users.py
@@ -0,0 +1,28 @@
+from locust import LoadTestShape
+from simple_test_01 import UserA, UserB
+
+class StagesShapeWithCustomUsers(LoadTestShape):
+    time_limit = 60 # 1-minutes
+    spawn_rate = 20
+
+    stages = [
+        {"duration": 10, "users": 10, "spawn_rate": 10, "user_classes": [UserA]},
+        {"duration": 30, "users": 50, "spawn_rate": 10, "user_classes": [UserA, UserB]},
+        {"duration": 60, "users": 100, "spawn_rate": 10, "user_classes": [UserB]},
+        {"duration": 120, "users": 100, "spawn_rate": 10, "user_classes": [UserA,UserB]},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                try:
+                    tick_data = (stage["users"], stage["spawn_rate"], stage["user_classes"])
+                    print('Success-01')
+                except:
+                    tick_data = (stage["users"], stage["spawn_rate"])
+                    print('Failure-01')
+                return tick_data
+
+        return None

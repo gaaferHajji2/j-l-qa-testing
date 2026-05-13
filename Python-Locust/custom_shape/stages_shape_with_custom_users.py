681e75d1cdb24f61ffe9852b6437c3c8e4c62864
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-13 17:33:26 +0300

    Using common options with parsed options

--- Python-Locust/custom_shape/stages_shape_with_custom_users.py
+++ Python-Locust/custom_shape/stages_shape_with_custom_users.py
@@ -4,6 +4,7 @@ from simple_test_01 import UserA, UserB
 class StagesShapeWithCustomUsers(LoadTestShape):
     time_limit = 60 # 1-minutes
     spawn_rate = 50
+    use_common_options = True
 
     stages = [
         {"duration": 10, "users": 10, "spawn_rate": 10, "user_classes": [UserA]},
@@ -16,6 +17,7 @@ class StagesShapeWithCustomUsers(LoadTestShape):
         run_time = self.get_run_time()
 
         for stage in self.stages:
+            # if run_time < self.runner.environment.parsed_options.run_time:
             if run_time < stage["duration"]:
                 try:
                     tick_data = (stage["users"], stage["spawn_rate"], stage["user_classes"])

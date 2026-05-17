08c4d1a5dc993f51f9b600e733ae33f3f2b83100
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-17 04:09:04 +0300

    Build simple file upload example

---
+++ Python-Pytest-Playwright/Tests-Playwright-Pytest-01/upload-file/upload_file.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwrite:
+    chrome = playwrite.chromium.launch(headless=False, slow_mo=1500)
+    page = chrome.new_page()
+    page.goto("https://bootswatch.com/cosmo/")
+
+    upload_file_item = page.get_by_label("Default file input example")
+    upload_file_item.scroll_into_view_if_needed()
+    upload_file_item.set_input_files("test.txt")
+
+    page.close()

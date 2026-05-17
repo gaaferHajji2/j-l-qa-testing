113ff93190289a8ca7491b2b18e18c59f52b975b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-17 04:09:57 +0300

    Adding A New Comment

--- Python-Pytest-Playwright/Tests-Playwright-Pytest-01/upload-file/upload_file.py
+++ Python-Pytest-Playwright/Tests-Playwright-Pytest-01/upload-file/upload_file.py
@@ -4,9 +4,9 @@ with sync_playwright() as playwrite:
     chrome = playwrite.chromium.launch(headless=False, slow_mo=1500)
     page = chrome.new_page()
     page.goto("https://bootswatch.com/cosmo/")
-
     upload_file_item = page.get_by_label("Default file input example")
     upload_file_item.scroll_into_view_if_needed()
+    # Here we can upload multiple files
     upload_file_item.set_input_files("test.txt")
 
     page.close()

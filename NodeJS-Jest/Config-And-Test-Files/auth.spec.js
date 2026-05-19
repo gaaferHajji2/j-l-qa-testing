e731771adcadbc0f3eeb979d92909f37518cc4d7
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-19 15:59:51 +0300

    Testing the checking jwt & user data

--- NodeJS-Jest/Config-And-Test-Files/auth.spec.js
+++ NodeJS-Jest/Config-And-Test-Files/auth.spec.js
@@ -32,7 +32,7 @@ describe("Test The Auth Module", () => {
         expect(mockNext).not.toHaveBeenCalled()
     })
 
-    it("should return missing token data", async () => {
+    it("should return authentication failed", async () => {
         let mockedRequest = mockRequest()
         mockedRequest.headers.authorization = "Bearer"
         let mockedResponse = mockResponse()
@@ -44,5 +44,22 @@ describe("Test The Auth Module", () => {
             error: "Authentication Failed",
         })
         expect(mockNext).not.toHaveBeenCalled()
+    })
+
+    it("should call the next function", async () => {
+        jest.spyOn(jwt, 'verify').mockResolvedValueOnce({ id: "64a1b2c3d4e5f6a7b8c9d0e1" })
+        jest.spyOn(User, 'findById').mockResolvedValueOnce({
+            id: "64a1b2c3d4e5f6a7b8c9d0e1",
+            name: "Jafar Loka",
+            email: "jloka@jloka.com",
+            password: "Hash@Test@123"
+        })
+        let mockedRequest = mockRequest()
+        let mockedResponse = mockResponse()
+
+        await isAuthenticatedUser(mockedRequest, mockedResponse, mockNext)
+
+        expect(mockedRequest).toHaveProperty('user')
+        expect(mockNext).toHaveBeenCalledTimes(1)
     })
 })

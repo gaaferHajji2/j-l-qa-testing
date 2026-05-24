12773d2088f4d79d843e7b4341f7d324fb0fb266
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-24 04:26:49 +0300

    Disable save to MySQL pipeline

--- Scrapy-Project/bookscraper/bookscraper/settings.py
+++ Scrapy-Project/bookscraper/bookscraper/settings.py
@@ -64,7 +64,7 @@ DOWNLOAD_DELAY = 1
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    "bookscraper.pipelines.BookscraperPipeline": 300,
-   "bookscraper.pipelines.SaveToMySQLPipeline": 400,
+#    "bookscraper.pipelines.SaveToMySQLPipeline": 400,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)

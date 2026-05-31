4978d86dad9bf247b84798c37e22a589caacb22c
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-31 16:41:47 +0300

    Remove unused whitespaces

--- Python-Pytest-Playwright/Tests-Playwright-Pytest-01/async-playwright/async_test_01.py
+++ Python-Pytest-Playwright/Tests-Playwright-Pytest-01/async-playwright/async_test_01.py
@@ -2,7 +2,6 @@ import asyncio
 from playwright.async_api import async_playwright
 
 async def main():
-
     async with async_playwright() as playwright:
         browser = await playwright.chromium.launch(headless=False, slow_mo=1000)
         page = await browser.new_page()

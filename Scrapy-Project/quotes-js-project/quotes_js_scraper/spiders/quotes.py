87aac58f96672e23e8932db732e749231eec4c6d
JafarLoka <gaafer.hajji1995@gmail.com>
2026-06-13 14:45:05 +0300

    Wait for an element by class to render

--- Scrapy-Project/quotes-js-project/quotes_js_scraper/spiders/quotes.py
+++ Scrapy-Project/quotes-js-project/quotes_js_scraper/spiders/quotes.py
@@ -1,13 +1,19 @@
 import scrapy
 from quotes_js_scraper.items import QuoteItem
 from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 class QuotesSpider(scrapy.Spider):
 	name = 'quotes'
 
 	def start_requests(self):
 		url = 'https://quotes.toscrape.com/js/'
-		yield SeleniumRequest(url=url, callback=self.parse)
+		yield SeleniumRequest(
+			url=url, 
+			callback=self.parse,
+			wait_until=EC.element_to_be_clickable((By.CLASS_NAME, 'quote'))
+		)
 
 	def parse(self, response):
 		quote_item = QuoteItem()

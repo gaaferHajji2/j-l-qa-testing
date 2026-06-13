fc2b6b259cde0ec360a795673b75a8b284d46007
JafarLoka <gaafer.hajji1995@gmail.com>
2026-06-13 14:35:33 +0300

    Testing the scraping for JS site

--- Scrapy-Project/quotes-js-project/quotes_js_scraper/spiders/quotes.py
+++ Scrapy-Project/quotes-js-project/quotes_js_scraper/spiders/quotes.py
@@ -1,11 +1,18 @@
 import scrapy
 from quotes_js_scraper.items import QuoteItem
-
+from scrapy_selenium import SeleniumRequest
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+  	name = 'quotes'
+	
+	def start_requests(self):
+		url = 'https://quotes.toscrape.com/js/'
+		yield SeleniumRequest(url = url, callback = self.parse)
 
-    def parse(self, response):
-        pass
+	def parse(self, response):
+    	quote_item = QuoteItem()
+		for quote in response.css('div.quote'):
+			quote_item['text'] = quote.css('span.text::text').get()
+			quote_item['author'] = quote.css('small.author::text').get()
+			quote_item['tags'] = quote.css('div.tags a.tag::text').getall()
+			yield quote_item
